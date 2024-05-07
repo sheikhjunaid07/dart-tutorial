59747276447f3b8a4bae101adeff94256d4c858d
myname <sheikhjunaid732000@gmail.com>
2024-05-07 18:20:46 +0530

    about types of inheritance

---
+++ OOPS/Inheritance/Single-Inheritance.dart
@@ -0,0 +1,28 @@
+class Person{
+  String? name;
+  int? age;
+  void printData(){
+    print("Name is: ${name}");
+    print("Age is: ${age}");
+  }
+}
+
+class Student extends Person{
+  String? schoolName;
+  String? city;
+
+  void printStudent(){
+    print("Student school name is: ${schoolName}");
+    print("Student city is: ${city}");
+  }
+}
+
+void main(){
+  Student student = new Student();
+  student.name = "Raj";
+  student.age = 18;
+  student.schoolName = "High School";
+  student.city = "Indore";
+  student.printData();
+  student.printStudent();
+}
